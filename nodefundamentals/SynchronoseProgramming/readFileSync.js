e3b0ecf6be2e5e96bd42ce99c82ed87114c4f960
keerthi.r <keerthi.r@thekaios.com>
2026-01-23 14:53:14 +0530

    featadded async and sync file reading

--- nodefundamentals/SynchronoseProgramming/readFileSync.js
+++ nodefundamentals/SynchronoseProgramming/readFileSync.js
@@ -3,4 +3,7 @@ const filePath ="../static/lengthyFile.txt";
 const start =Date.now();
 const data = fs.readFileSync(filePath,'utf-8');
 
-console.log(`File read in ${Date.now - start} ms`);
+console.log(`File reading completed in ${Date.now() - start} ms`);
+for(let i=0;i<10;i++){
+    console.log("doing work",i);
+}
