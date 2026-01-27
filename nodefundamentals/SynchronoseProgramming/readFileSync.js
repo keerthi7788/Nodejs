f11103b9ddfdddff3f6cb2c6c7379eba389377d8
keerthi.r <keerthi.r@thekaios.com>
2026-01-27 12:42:01 +0530

    commit

--- nodefundamentals/SynchronoseProgramming/readFileSync.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs')
-const filePath ="../static/lengthyFile.txt";
-const start =Date.now();
-const data = fs.readFileSync(filePath,'utf-8');
-
-console.log(`File reading completed in ${Date.now() - start} ms`);
-for(let i=0;i<10;i++){
-    console.log("doing work",i);
-}
